5d1ef6cdd63a0b20af3d40a4858032fb1305bdc9
squhart <squhart@gmail.com>
2015-04-28 15:50:23 +0700

    Initial Commit
    Files Upload Using Dropzone.js

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+ 
+<head>   
+	<link href="css/dropzone.css" type="text/css" rel="stylesheet" />
+	<script src="dropzone.js"></script>
+</head>
+ 
+<body>
+	<form action="upload.php" class="dropzone"></form>
+</body>
+ 
+</html>
